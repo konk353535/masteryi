85904b96245bce1d2fca89e02aaadd59f9438766
Jason Romaior <jason.romaior@safetyculture.io>
2016-04-24 14:58:39 +1000

    [Backend] Leaderboard route

--- backend/routes/stats.js
+++ backend/routes/stats.js
@@ -1,9 +1,9 @@
-var env = process.argv[2] || 'prod';
-var config = require('./../config')(env);
-var logger = require('bunyan').createLogger({ name: "statRoutes "});
-var pg = require('pg');
+const env = process.argv[2] || 'prod';
+const config = require('./../config')(env);
+const logger = require('bunyan').createLogger({ name: 'statRoutes '});
+const pg = require('pg');
 pg.defaults.parseInt8 = true;
-var client = new pg.Client(config.database);
+const client = new pg.Client(config.database);
 client.connect();
 
 module.exports = function (app) {
@@ -13,4 +13,17 @@ module.exports = function (app) {
       res.send(result.rows);
     });
   });
-}
+
+  app.get('/api/summary', (req, res) => {
+    const queryText = 'SELECT * FROM mastery, users WHERE mastery.points IN ' +
+      '(select max(points) FROM mastery GROUP BY champion_id) AND users.id = mastery.user_id';
+    client.query(queryText, (err, result) => {
+      if (err) {
+        logger.error(err);
+        return res.status(500).send('Uhh woops, that\'s a problem');
+      }
+
+      res.send(result);
+    });
+  });
+};
