132c5cd2bfd82ec36b545428e8494480bc179581
Jason Romaior <jason.romaior@safetyculture.io>
2016-05-12 12:24:58 +1000

    [Backend] Update util to scan additional regions

--- backend/util.js
+++ backend/util.js
@@ -3,6 +3,14 @@ const getPlatformID = function (serverID){
   if(serverID.toLowerCase() === 'na') return 'NA1'
   if(serverID.toLowerCase() === 'euw') return 'EUW1'
   if(serverID.toLowerCase() === 'eune') return 'EUN1'
+  if(serverID.toLowerCase() === 'jp') return 'JP1'
+  if(serverID.toLowerCase() === 'kr') return 'KR'
+  if(serverID.toLowerCase() === 'lan') return 'LA1'
+  if(serverID.toLowerCase() === 'las') return 'LA2'
+  if(serverID.toLowerCase() === 'tr') return 'TR1'
+  if(serverID.toLowerCase() === 'ru') return 'RU'
+  if(serverID.toLowerCase() === 'br') return 'BR1'
+
 }
 
 const toSingleObject = function (arr, key){
